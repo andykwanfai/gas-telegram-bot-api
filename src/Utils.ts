c61acbb954962fa931d8a05ebcabf07ff99e2b16
Andy KWAN <andykwanfai@gmail.com>
2024-02-24 12:56:37 +0800

    Utils support non gas env

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,4 @@
-
+import moment from "moment-timezone";
 export class Utils {
   static parseJson(s: string): null | object {
     try {
@@ -9,7 +9,12 @@ export class Utils {
   }
 
   static sleep(sec: number) {
-    Utilities.sleep(sec * 1000);
+    if (Utilities) {
+      Utilities.sleep(sec * 1000);
+    } else {
+      const start = new Date().getTime();
+      while (new Date().getTime() - start < sec * 1000);
+    }
   }
 
   static now() {
@@ -17,6 +22,10 @@ export class Utils {
   }
 
   static getJSTDateString(date: Date) {
-    return Utilities.formatDate(date, 'Asia/Tokyo', "yyyy-MM-dd HH:mm:ss z");
+    if (Utilities) {
+      return Utilities.formatDate(date, 'Asia/Tokyo', "yyyy-MM-dd HH:mm:ss z");
+    }
+    const formattedDate = moment(date).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm:ss z')
+    return formattedDate;
   }
 }
